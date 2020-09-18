baed09f0919358e619cf1efc76eb5278dcbd6cd7
Alan Coppola <ajjcoppola@gmail.com>
2020-09-17 21:29:38 -0700

    Initial Resources from Blog articles by agarner
    Each dir is the resources: Notebook, Sim, Live files for a specific strategy.  This original matches up with agarner blog articles.

---
+++ HCA_Fixed_Ratio_Allocations/HCA_Fixed_Ratio_Allocations_Live.py
@@ -0,0 +1,63 @@
+# HCA Live-Trade Conversion: Date:2020-08-13
+# HCA Live-Simulation: Conversion Date: 08-13-2020
+# Updated 09-03-2020
+# Conversion Author: Anthony Garner
+
+# HCA Original Code Source: Quantopian, various
+# ------------ Start: HCA: zipline includes ------------------ 
+from zipline.api import symbol, get_open_orders, order, order_target_percent
+import numpy as np
+
+# Start:Zipline Builtin Functions
+def initialize(context):
+    pass
+
+def handle_data(context, data):
+    if (not context.ORDERS_DONE):
+        context.ORDERS_DONE = True
+        trade(context, data) #Name of the original algo trading function
+    else:
+        print("Exiting: Zipline-broker: context.portfolio : {}".format(context.portfolio))
+        exit()
+def before_trading_start(context, data):
+    c = context
+    c.ORDERS_DONE       = False #No Orders done yet today
+    c.all_orders = {}        
+# End:Zipline Builtin Functions
+
+def trade(context,data):
+    stocks     = [symbol('QQQ'), symbol('XLP'),symbol('IEF'),symbol('TLT')]
+    proportion = [0.25, 0.25, 0.25, 0.25]
+    lev        = 0.9 #Should allow for a small percentage of Cash, to enable ordering fluctuations without having order cancelled.
+
+    ### ajjc: Find a way to return if already traded today
+    print("TradingLinkData: Zipline-broker: context.portfolio : {}".format(context.portfolio))
+    print("TradingLinkData: IB-Account    : context.account   : {}".format(context.account))
+
+    acct_liq    = context.portfolio.starting_cash #Same as IB net_liquidation
+    acct_invest = lev * acct_liq
+    positions      = context.broker.positions
+    
+    # Sell any existing positions which are not in stocks
+    for key in positions:
+        if (key not in stocks and not get_open_orders(key)):
+            order(key, -positions[key].amount)  
+            
+    # Loop through stocks and deal with the list according to whether there are existing positions or not
+    for i in range(len(stocks)):
+        if data.can_trade(stocks[i]) and not get_open_orders(stocks[i]):
+            # If there is a position already in this stock, then rebalance it if necessary in accordance with proportion
+            if stocks[i] in positions:
+                current_amt = positions[stocks[i]].amount
+                rebalance_amt = int(acct_invest*proportion[i] / data.current(stocks[i],'price'))
+                delta_amt = rebalance_amt - current_amt
+                if delta_amt != 0:
+                    order(stocks[i], delta_amt) 
+                else:
+                    print("No new orders for : {}".format(stocks[i]))
+            # If there is no existing position in the stock, take one
+            if (stocks[i] not in positions and data.can_trade(stocks[i]) 
+                and not get_open_orders(stocks[i])):
+                amt = int(acct_invest*proportion[i] / data.current(stocks[i],'price'))
+                if amt != 0:
+                    order(stocks[i], amt)                                                
---
+++ HCA_Trendfollowing_live/HCA_Trendfollowing_live.py
@@ -0,0 +1,101 @@
+# HCA Live-Trade Conversion: Date:2020-08-13
+# HCA Live-Simulation: Conversion Date: 08-13-2020
+# Amended in Accordance with decision to use order: 2020-08-29
+# Conversion Author: Anthony Garner
+
+from zipline.api import symbol,symbols, get_open_orders, order
+import numpy as np
+import pandas as pd
+
+# Start:Zipline Builtin Functions
+def initialize(context):
+    context.asserts = symbols('SPY','ZSL', 'KOLD', 'GLD')
+    context.bonds = symbol('SHY')
+    context.top_n_by_momentum = pd.Series() 
+    #Choose X stocks out of portfolio of Y stocks- how many stocks to hold - top X by momentum 
+    context.num_stocks=3
+    #Lookback for momentum calculation
+    context.momentum_days=60
+    #Set context.trend to 0 to reject any stocks with negative momentum, set to -1 to accept stocks with negative momentum
+    context.trend =0.0
+    context.stocks=[] 
+
+    # Compute momentum
+def compute_momentum(context,data):  
+    price_history = data.history(context.asserts, "price", context.momentum_days+5, "1d")
+    momentum = price_history.pct_change(context.momentum_days).iloc[-1]
+    context.top_n_by_momentum = momentum.nlargest(context.num_stocks).where(momentum>context.trend).dropna()
+    return context.top_n_by_momentum
+        
+def handle_data(context, data):
+    if (not context.ORDERS_DONE):
+        context.ORDERS_DONE = True
+        trade(context, data) #Name of the original algo trading function
+    else:
+        print("Exiting: Zipline-broker: context.portfolio : {}".format(context.portfolio))
+        exit()
+        
+def before_trading_start(context, data):
+    c = context
+    c.ORDERS_DONE       = False #No Orders done yet today
+    c.all_orders = {} 
+    compute_momentum(context, data)
+    context.stocks=[]
+    for index,value in context.top_n_by_momentum.items():
+            context.stocks.append(index)
+    context.stocks.append(context.bonds)
+# End:Zipline Builtin Functions
+
+def trade(context,data):
+    lev = 0.9 #Should allow for a small percentage of Cash, to enable ordering fluctuations without having order cancelled.
+    stocks = context.stocks
+    num_stocks = context.num_stocks
+    ### ajjc: Find a way to return if already traded today
+    print("TradingLinkData: Zipline-broker: context.portfolio : {}".format(context.portfolio))
+    print("TradingLinkData: IB-Account    : context.account   : {}".format(context.account))
+    acct_liq    = context.portfolio.starting_cash #Same as IB net_liquidation
+    acct_invest = lev * acct_liq   
+    positions      = context.broker.positions   
+    
+    for key in positions:
+        if (key not in stocks and key != context.bonds and not get_open_orders(key)):
+            order(key, -positions[key].amount)      
+        
+    for i in range(len(stocks)):
+        if data.can_trade(stocks[i]) and not get_open_orders(stocks[i]) and stocks[i] != context.bonds:
+            
+            if stocks[i] in positions:
+                
+                current_amt = positions[stocks[i]].amount
+                rebalance_amt = int(acct_invest*(1/num_stocks) / data.current(stocks[i],'price'))
+                delta_amt = rebalance_amt - current_amt
+                
+                if delta_amt != 0:
+                    order(stocks[i], delta_amt) 
+                else:
+                    print("No new orders for : {}".format(stocks[i]))
+            
+            if (stocks[i] not in positions and data.can_trade(stocks[i]) 
+                and not get_open_orders(stocks[i]) and stocks[i] != context.bonds):
+                
+                amt = int(acct_invest*(1/context.num_stocks) / data.current(stocks[i],'price'))
+                order(stocks[i], amt)                                                
+        
+        if data.can_trade(stocks[i]) and not get_open_orders(stocks[i]) and stocks[i] == context.bonds: 
+            
+            rebalance_amt = int(acct_invest*(1-(1/context.num_stocks)*(len(stocks)-1)) / data.current(stocks[i],'price'))
+            
+            if stocks[i] in positions:  
+                
+                current_amt = positions[stocks[i]].amount
+                delta_amt = rebalance_amt - current_amt
+                
+                if delta_amt != 0:
+                    order(stocks[i], delta_amt)
+                else:
+                    print("No new orders for : {}".format(stocks[i]))    
+            else:
+                
+                if rebalance_amt > 0:
+                    order(stocks[i], rebalance_amt)
+            
